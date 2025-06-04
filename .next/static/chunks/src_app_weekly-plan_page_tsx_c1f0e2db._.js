528ea763052ae2d012f938ed2072828c28bae7b1
Jay Kamat <jkamat01@gmail.com>
2025-06-04 12:11:39 +0530

    Add .next/static/chunks/src_app_weekly-plan_page_tsx_c1f0e2db._.js 1749019295023

---
+++ .next/static/chunks/src_app_weekly-plan_page_tsx_c1f0e2db._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_6019879d._.js",
+  "static/chunks/src_0245b2be._.js"
+],
+    source: "dynamic"
+});
