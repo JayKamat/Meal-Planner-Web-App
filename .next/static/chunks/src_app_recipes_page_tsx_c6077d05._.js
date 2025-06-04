784ab53297d23a926ee650f8b124938283a53d8e
Jay Kamat <jkamat01@gmail.com>
2025-06-04 12:11:46 +0530

    Add .next/static/chunks/src_app_recipes_page_tsx_c6077d05._.js 1749019302289

---
+++ .next/static/chunks/src_app_recipes_page_tsx_c6077d05._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_2c1cb36d._.js",
+  "static/chunks/src_65b3b061._.js"
+],
+    source: "dynamic"
+});
