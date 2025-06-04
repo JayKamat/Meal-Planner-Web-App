514a872b1fa2fd51bfac01ed286b5fd91d61d2c1
Jay Kamat <jkamat01@gmail.com>
2025-06-04 12:11:51 +0530

    Add .next/static/chunks/src_app_inventory_page_tsx_c1f0e2db._.js 1749019307317

---
+++ .next/static/chunks/src_app_inventory_page_tsx_c1f0e2db._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_9c2e955e._.js",
+  "static/chunks/src_343d7d23._.js"
+],
+    source: "dynamic"
+});
